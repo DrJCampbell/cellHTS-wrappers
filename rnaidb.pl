5589fd3d43dae44c20a80c70e6ea128dae76dcc9
James Campbell <Dr.J.Campbell@gmail.com>
2015-10-13 19:56:51 +0100

    added lots of template files to ./lib. Most need further changes to hook into the cgi script variables. rnaibd.pl has the start of a template call.

--- rnaidb.pl
+++ rnaidb.pl
@@ -21,6 +21,13 @@ no strict 'refs';	# CHECK IF NEEDED
 					# use string ("test_plateconf.txt") as a symbol ref
 					# while "strict refs" in use at...
 
+
+# Don't buffer output
+$| = 1;
+
+# print an http header
+print "Content-type: text/html\n\n";
+
 my $q = new CGI;
 
 
@@ -29,8 +36,6 @@ my $ISOGENIC_SET;
 my $ADD_NEW_FILES_LINK = $config_hash{'hostname'} . "cgi-bin/$script_name?add_new_files=1";
 
 
-# Don't buffer output
-$| = 1;
 
 # get script and config filenames, then read config into hash
 my $script_name = &get_script_name($0);
@@ -91,4 +96,95 @@ else {
 }
 
 
+sub add_new_screen{
+  
+  # file_upload_message can be passed to the script from the upload new file section(s)
+  my $file_upload_message = $q -> param ( "file_upload_message" );
+  
+  # links to existing cellHTS input files
+  my $plate_list_download_link = $config_hash{'hostname'} . $config_hash{'platelist_folder'};
+  
+  # link to add new cellHTS input files
+  my $link_to_add_new_files = $config_hash{'hostname'} . "cgi-bin/$script_name?add_new_files=1";
+  
+  #
+  # Multiple database queries here.
+  #
+  
+  # get the existing tissue type from the database
+  my $tissueType;
+  my @tissueType;
+  my $query = "SELECT Tissue_of_origin FROM Tissue_type order by Tissue_of_origin";
+  my $query_handle = $dbh -> prepare ( $query )
+    or die "Cannot prepare: " . $dbh -> errstr();
+  $query_handle->execute();
+  while ( $tissueType = $query_handle -> fetchrow_array ){
+    push ( @tissueType, $tissueType );  
+  }
+  
+  # get the existing cell line names from the database
+  my $cellLineName;
+  my @cellLineName;
+  $query = "SELECT Cell_line_name FROM Cell_line order by Cell_line_name";
+  $query_handle = $dbh -> prepare ( $query )
+    or die "Cannot prepare: " . $dbh -> errstr();
+  $query_handle->execute();
+  while ( $cellLineName = $query_handle -> fetchrow_array ){  
+    push ( @cellLineName, $cellLineName );
+  }
+  
+  # get plate list file paths from the database
+  $query = "SELECT Platelist_file_location FROM Platelist_file_path";
+  $query_handle = $dbh -> prepare ( $query )
+    or die "Cannot prepare: " . $dbh -> errstr();
+  $query_handle->execute()
+    or die "SQL Error: " . $query_handle -> errstr();
+  my $platelist_name;
+  my @platelist_path;
+  while ( $platelist_name = $query_handle -> fetchrow_array ) {
+    $platelist_name =~ s/.*\///;
+    $platelist_name =~ s/\.[^.]+$//;
+    push ( @platelist_path, $platelist_name );
+  }
+  unshift ( @platelist_path, "Please select" );
+  
+  # get template file paths from the database
+  $query = "SELECT Template_library_file_location FROM Template_library_file_path";
+  $query_handle = $dbh -> prepare ( $query )
+    or die "Cannot prepare: " . $dbh -> errstr();
+  $query_handle->execute()
+    or die "SQL Error: " . $query_handle -> errstr();
+  my $templib_name;
+  my @templib_path;
+  while ( $templib_name = $query_handle -> fetchrow_array ) {
+    $templib_name =~ s/.*\///;
+    $templib_name =~ s/\.[^.]+$//;
+    push ( @templib_path, $templib_name );
+  }
+  unshift( @templib_path, "Please select" );
 
+  # If we are finished fetching DB stuff we can close the qh
+  $query_handle -> finish();
+  
+  
+  my $vars = {
+    'file_upload_message' => $file_upload_message,
+    'plate_list_download_link' => $plate_list_download_link,
+    'link_to_add_new_files' => $link_to_add_new_files,
+    'tissueType' => @tissueType,
+    'cellLineName' => @cellLineName,
+    'platelist_path' => @platelist_path,
+    'templib_path' => @templib_path,
+  };
+  
+  my $template = Template->new({
+  	INCLUDE_PATH => ${} . './lib',
+  	PRE_PROCESS => 'config',
+  });
+  
+  $template->process($vars) || die $template->error();
+  
+  
+  
+  
+}
