fea73f7d0f04c0d03e86abbf89103b5742c6f164
James Campbell <Dr.J.Campbell@gmail.com>
2015-10-13 15:29:50 +0100

    Added get_script_name, get_conf_file, get_config_hash functions to pm and script header to rnaidb.pl

--- Cellhtswrappers.pm
+++ Cellhtswrappers.pm
@@ -36,7 +36,7 @@ Rahul Kumar
 # begin code
 package Cellhtswrappers;
 use  base qw(Exporter);
-our @EXPORT = qw(my_func_1 my_func_2);
+our @EXPORT = qw(get_script_name get_conf_file);
 use Carp;
 
 #
@@ -45,19 +45,40 @@ use Carp;
 
 my $shared_stuff = '';
 
-#
-sub my_func_1{
-  # do something...
-  my $something = "something";
-  return $something;
+sub get_script_name($){
+  # strip path to script name and return
+  my $path = shift;
+  $path =~ /([^\/]+)$/;
+  my $script_name = $1;
+  warn unless $script_name =~ /[a-z0-9_]\.(pl|r)/i;
+  return $script_name;
 }
 
-sub my_func_2 {
-  # do something else...
-  my $something_else = "something";
-  return $something_else;
-
+sub get_conf_file($){
+  # replace the script name extention to find the conf file
+  my $script_name = shift;
+  my $conf_file = $script_name;
+  $conf_file =~ s/pl$/conf/;
+  return $conf_file;
 }
 
+sub get_config_hash{
+  my $conf_file = shift;
+  my %config;
+  open CONFIG, "< $conf_file" or die "Can't read list of configurations from file $conf_file: $!\n";
+  while(<CONFIG>){
+    next if /^(#|[\n\r])/;
+    if(/^([^\t]+)\t+([^\t]+)/){
+      my ($key, $value) = ($1, $2);
+      chomp($value);
+      if(exists($config{$key})){
+        die "configure: $key is duplicated in the config file $conf_file\n\nYou need to fix that to continue\n";
+      }
+      $config{$key} = $value;
+    }
+  }
+  return %config;
+}
 
+# This last line is required to stay here
 1;
